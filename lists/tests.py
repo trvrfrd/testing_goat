4d73563155d2e1d21cc77534c54aaf483c1c56bb
Trevor Ford <info@trvrfrd.net>
2021-01-29 18:13:51 -0500

    add List model, and foreign key on Item

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -34,8 +34,9 @@ class ListViewTest(TestCase):
 
 
     def test_displays_all_items(self):
-        Item.objects.create(text="item 1")
-        Item.objects.create(text="item 2")
+        list_ = List.objects.create()
+        Item.objects.create(text="item 1", list=list_)
+        Item.objects.create(text="item 2", list=list_)
 
         response = self.client.get("/lists/the-only-list-in-the-world/")
 
@@ -43,21 +44,32 @@ class ListViewTest(TestCase):
         self.assertContains(response, "item 2")
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = "The first (ever) list item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "The second item"
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list, list_)
+
+        second_saved_item = saved_items[1]
         self.assertEqual(second_saved_item.text, "The second item")
+        self.assertEqual(first_saved_item.list, list_)
