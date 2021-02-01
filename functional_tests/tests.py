b47bec9644cd512ad84f4479c952841646575dd7
Trevor Ford <info@trvrfrd.net>
2021-02-01 11:02:51 -0500

    new URL + view for adding Items to existing Lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -98,13 +98,13 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table("1: Buy milk")
 
         # Francis gets his own unique URL
-        francis_list_url = self.browser.get(self.browser.current_url)
+        francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, "/lists/.+")
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name("body").text
         self.assertNotIn("wash my face", page_text)
-        self.assertNotIn("brush my teeth", page_text)
+        self.assertIn("Buy milk", page_text)
 
         # everyone is happy forever
