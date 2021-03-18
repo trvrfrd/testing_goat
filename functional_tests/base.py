cd609138100349105493e818aef7f078d739303c
Trevor Ford <info@trvrfrd.net>
2021-03-18 15:47:52 -0400

    add generic wait_for helper method to FunctionalTest and fill out FT for blank list item input validation

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -23,9 +23,19 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser.quit()
 
 
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.1)
+
+
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
-
         while True:
             try:
                 table = self.browser.find_element_by_id("id_list_table")
