d647d7b641e7ada63544a20ed262393ab6d4d450
Trevor Ford <info@trvrfrd.net>
2021-03-28 20:21:13 -0400

    add new Form for adding Items to an existing List with uniqueness validation

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.utils.html import escape
+from unittest import skip
 
 from lists.views import home_page
 from lists.models import Item, List
@@ -140,6 +141,22 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        list2 = Item.objects.create(list=list1, text="hello")
+        response = self.client.post(
+            f"/lists/{list1.id}/",
+            data={"text": "hello"}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, "list.html")
+        self.assertEqual(Item.objects.all().count(), 1)
+
 
     def test_displays_item_form(self):
         list_ = List.objects.create()
